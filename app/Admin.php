ba4c0fb9b0785c97b757b38257b62c28685520a5
Habibur Rahman <habibblog97@gmail.com>
2024-01-06 19:40:00 +0600

    lince key api created

--- app/Admin.php
+++ app/Admin.php
@@ -9,6 +9,23 @@ class Admin {
      */
     public function __construct() {
         add_action( 'rest_api_init', [$this, 'bsc_license_api_endpoint'] );
+        add_action( 'woocommerce_new_order', [$this, 'save_license_data'], 10, 1 );
+
+        add_action( 'init', [$this, 'init'] );
+    }
+
+    public function init() {
+        // echo md5(1);
+    }
+
+    public function save_license_data( $order_id ) {
+
+        $order      = wc_get_order($order_id);
+        $user_id    = $order->get_user_id();
+
+        $license_key = md5( $user_id ) . $user_id;
+
+        update_user_meta( $user_id, 'bsc_license_key', $license_key );
     }
 
     /**
@@ -28,12 +45,22 @@ class Admin {
 
         $license_key = sanitize_text_field( $request->get_param('license_key') );
 
-        $response = array(
-            'license_key' => $license_key,
-            'status'    => 'success', 
-            'message'   => 'Verify License key.',
-        );
-    
+        $user_id  = substr( $license_key, 32 );
+
+        $user_meta_key = get_user_meta( $user_id, 'bsc_license_key', true );
+
+        if( $license_key == $user_meta_key ) {
+            $response = array(
+                'status'    => 'success', 
+                'message'   => 'Verify License key.',
+            );
+        }else{
+            $response = array(
+                'status' => 'error', 
+                'message' => 'Invalid license key'
+            );
+        }
+
         return rest_ensure_response($response);
     }
 
