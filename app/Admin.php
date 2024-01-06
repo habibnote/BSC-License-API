2109d335e9a57a5df1b993c1e621a166dc514441
Habibur Rahman <habibblog97@gmail.com>
2024-01-06 18:39:34 +0600

    Register a api endpoint

--- app/Admin.php
+++ app/Admin.php
@@ -8,11 +8,29 @@ class Admin {
      * Class Constructor
      */
     public function __construct() {
-        add_action( 'init', [$this, 'init'] );
+        add_action( 'rest_api_init', [$this, 'bsc_license_api_endpoint'] );
     }
 
-    public function init() {
-        echo "Hello";
+    /**
+     * Register Custom Enpoint
+     */
+    public function bsc_license_api_endpoint() {
+        register_rest_route('bsc/v1', '/post-data/', array(
+            'methods'   => 'POST',
+            'callback'  => [$this, 'handle_post_data'],
+        ));
+    }
+
+    /**
+     * API Callback function 
+     */
+    public function handle_post_data( $data ) {
+        $response = array(
+            'status' => 'success',
+            'message' => 'Data received successfully.',
+        );
+    
+        return rest_ensure_response( $response );
     }
 
 }
